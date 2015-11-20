63a97351883ea664bc1cc511cfecd0d5c5c0ade8
Dominic Waterson <dominic.waterson@tombola.com>
2015-11-20 09:59:00 +0000

    got rid of testing comments

--- main-app/app/scripts/bingoController/gameController.js
+++ main-app/app/scripts/bingoController/gameController.js
@@ -17,7 +17,6 @@
                     proxy.buyTicket(tokenService.getToken(), userModel.currentBalance, userModel.name).then(function (response) {
                         userModel.userBoughtTicket(response.balance);
                         bingoModel.getTicket(response.ticket);
-                        //bingoModel.getTicket('011722475204365360702637497481233455758302154058881928446789061241507324334876840738576186051132437816395663800818206590104559628214294664710935667287132130687703253151692742547985');
                     });
                 }
             };
